703bad2792b35ffb136d381105f3f55fe5c3ff08
VOIDED <bmackmrhs@gmail.com>
2025-08-08 19:51:40 -0400

    Added SQL logic for clubs_routes

---
+++ api/backend/clubs/clubs_routes.py
@@ -0,0 +1,45 @@
+########################################################
+# Sample customers blueprint of endpoints
+# Remove this file if you are not using it in your project
+########################################################
+
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+
+#------------------------------------------------------------
+# Create a new Blueprint object, which is a collection of 
+# routes.
+clubs = Blueprint('clubs', __name__)
+
+#------------------------------------------------------------
+# Get all the club from the database, package them up,
+# and return them to the client
+@clubs.route('/clubs', methods=['GET'])
+def get_clubs():
+    query = '''
+        SELECT name AS club_name
+        FROM clubs ORDER BY name
+    '''
+    
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute(query)
+
+    # fetch all the data from the cursor
+    # The cursor will return the data as a 
+    # Python Dictionary
+    theData = cursor.fetchall()
+
+    # Create a HTTP Response object and add results of the query to it
+    # after "jasonify"-ing it.
+    response = make_response(jsonify(theData))
+    # set the proper HTTP Status code of 200 (meaning all good)
+    response.status_code = 200
+    # send the response back to the client
+    return response
