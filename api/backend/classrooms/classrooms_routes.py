2bcb72f78e26869a124a37264c4ad106f74e6f30
Juri5ka <okamoto.ju@northeastern.edu>
2025-08-10 14:49:53 -0400

    updated the logging thing for classrooms since they have room numbers instead of userIds?

--- api/backend/classrooms/classrooms_routes.py
+++ api/backend/classrooms/classrooms_routes.py
@@ -45,7 +45,7 @@ def get_students():
 
     # Another example of logging for debugging purposes.
     # You can see if the data you're getting back is what you expect. 
-    current_app.logger.info(f'GET /classrooms/<userId> Result of query = {theData}')
+    current_app.logger.info(f'GET /classrooms/<roomNumber> Result of query = {theData}')
 
     # Create a HTTP Response object and add results of the query to it
     # after "jasonify"-ing it.
