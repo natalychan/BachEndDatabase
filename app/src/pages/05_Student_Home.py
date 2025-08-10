323362d24690307f5943e49c9efaf1e937eec89c
lin.xiangru <lin.xiangru@outlook.com>
2025-08-10 16:36:10 -0400

    Updated buttons in student home page

--- app/src/pages/05_Student_Home.py
+++ app/src/pages/05_Student_Home.py
@@ -14,13 +14,35 @@ st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
+
 #change all of these to be appropriate for the student
-if st.button('View Demographic Data Visualization', 
+
+#page shows student schedule, and there's gpa under each class
+if st.button('Schedule', 
              type='primary',
              use_container_width=True):
   st.switch_page('pages/01_World_Bank_Viz.py')
 
-if st.button('View Student Performance Data Visualization', 
+#page shows instrument rentals info and instrument vacancy
+if st.button('Instrument Rentals', 
              type='primary',
              use_container_width=True):
   st.switch_page('pages/02_Map_Demo.py')
+
+#page shows classroom booking info and classroom vacancy
+if st.button('Classroom Bookings', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/02_Map_Demo.py')  
+
+#page shows advisor meetings
+if st.button('Advisor Meetings', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/02_Map_Demo.py')  
+
+#page shows clubs info
+if st.button('Clubs', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/02_Map_Demo.py')    
