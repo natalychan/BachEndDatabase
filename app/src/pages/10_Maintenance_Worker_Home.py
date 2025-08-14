f8c33c50617f2dd8e29253647513d673f9768e3a
natalychan <chan.natal@northeastern.edu>
2025-08-14 13:56:36 -0400

    fixed headers

--- app/src/pages/10_Maintenance_Worker_Home.py
+++ app/src/pages/10_Maintenance_Worker_Home.py
@@ -15,17 +15,21 @@ st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
+
 if st.button('Daily Maintenance Tasks', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/11_Maintenance_Worker_Home.py')
+  st.switch_page('pages/11_Maintenance_Worker_Home.py',
+  st.header('Daily Maintenance Tasks'))
 
 if st.button('Track Work Hours and Pay', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/12_Hours_Wage_Pay.py')
+  st.switch_page('pages/12_Hours_Wage_Pay.py',
+  st.header('Track Work Hours and Pay'))
 
 if st.button("System Admin Privileges",
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/13_System_Admin_Privileges.py')
+  st.switch_page('pages/13_System_Admin_Privileges.py',
+  st.header('System Admin Privileges'))
