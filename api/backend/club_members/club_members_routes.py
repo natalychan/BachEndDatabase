31796817bb8547684950c62418fcb1e4390a57e3
natalychan <chan.natal@northeastern.edu>
2025-08-14 17:35:02 -0400

    turns out the att for clubs_members actually was studentId...

--- api/backend/club_members/club_members_routes.py
+++ api/backend/club_members/club_members_routes.py
@@ -6,14 +6,14 @@ club_members_api = Blueprint('club_members_api', __name__)
 
 # GET /api/students/<student_id>/clubs
 @club_members_api.route('/club_members', methods=['GET'])
-def club_mems(userId):
+def club_mems(studentId):
     query = """
         SELECT cm.clubName AS club_name
         FROM club_members cm
-        WHERE cm.userId = %s
+        WHERE cm.studentId = %s
     """
     cursor = db.get_db().cursor()
-    cursor.execute(query, (userId))
+    cursor.execute(query, (studentId))
     theData = cursor.fetchall()
     response = make_response(jsonify(theData))
     response.status_code = 200
