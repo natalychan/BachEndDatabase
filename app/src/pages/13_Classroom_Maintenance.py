0dc23f6170151a969328793773f89f3c36796303
lin.xiangru <lin.xiangru@outlook.com>
2025-08-14 17:50:52 -0400

    (removed echo)

--- app/src/pages/13_Classroom_Maintenance.py
+++ app/src/pages/13_Classroom_Maintenance.py
@@ -16,22 +16,22 @@ st.header('Classroom Maintenance')
 st.write(f"### Hi, {st.session_state['first_name']}, these classrooms are due for maintenance!")
 
 
-with st.echo(code_location='above'):
-    try:
-        API_URL = "http://web-api:4000/api/classrooms"
-        response = requests.get(API_URL)
-        
-        if response.status_code == 200:
-            data = response.json()
-            
-            if data:
-                df = pd.DataFrame(data)
-                st.dataframe(df, use_container_width=True)
-                st.info(f"Classrooms Needing Maintenance: {len(df)}")
-            else:
-                st.warning("No classrooms need maintenance")
-        else:
-            st.error(f"Failed to fetch data: HTTP {response.status_code}")
 
-    except Exception as e:
+try:
+    API_URL = "http://web-api:4000/api/classrooms"
+    response = requests.get(API_URL)
+        
+    if response.status_code == 200:
+        data = response.json()
+            
+        if data:
+            df = pd.DataFrame(data)
+            st.dataframe(df, use_container_width=True)
+            st.info(f"Classrooms Needing Maintenance: {len(df)}")
+        else:
+            st.warning("No classrooms need maintenance")
+    else:
+        st.error(f"Failed to fetch data: HTTP {response.status_code}")
+
+except Exception as e:
         st.error(f"Error: {str(e)}")
