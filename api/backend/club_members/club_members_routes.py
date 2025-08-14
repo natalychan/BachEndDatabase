46bf1bac621cafe81946edd6ed8373deff737db2
natalychan <chan.natal@northeastern.edu>
2025-08-14 18:56:52 -0400

    stil fixing clubs..

--- api/backend/club_members/club_members_routes.py
+++ api/backend/club_members/club_members_routes.py
@@ -5,18 +5,18 @@ from backend.db_connection import db
 club_members_api = Blueprint('club_members_api', __name__)
 
 # GET /api/students/<student_id>/clubs
-@club_members_api.route('/club_members/<int:studentId/clubs', methods=['GET'])
+@club_members_api.route('/club_members/<int:studentId>', methods=['GET'])
 def club_mems(studentId):
     query = """
         SELECT cm.clubName AS club_name
         FROM club_members cm
         WHERE cm.studentId = %s
     """
-    current_app.logger.info("GET /club_members/%s/clubs", studentId)
+    current_app.logger.info("GET /club_members/%s", studentId)
     cursor = db.get_db().cursor()
     cursor.execute(query, (studentId,))
     theData = cursor.fetchall()
-    response = make_response(jsonify(theData))
+    current_app.logger.info("GET /club_members/%s : rows=%d", studentId, len(theData))
+    response = make_response(jsonify(theData[0] if theData else {}))
     response.status_code = 200
     return response
-
