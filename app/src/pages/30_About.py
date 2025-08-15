00c673c782f9c5a52173b2db4f200cd23cd317f0
VOIDED <bmackmrhs@gmail.com>
2025-08-15 19:14:13 -0400

    Quick update to about page

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -8,11 +8,8 @@ st.write("# About this App")
 
 st.markdown (
     """
-    This is a demo app for CS 3200 Course Project.  
+    Welcome to the Join U Portal!
 
-    The goal of this demo is to provide information on the tech stack 
-    being used as well as demo some of the features of the various platforms. 
-
-    Stay tuned for more information and features to come!
+    Here you will be able to access all of your role specific tools.
     """
         )
