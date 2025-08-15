2e24904a385834029a5b6d73bb7d7caffc27e4b4
VOIDED <bmackmrhs@gmail.com>
2025-08-15 19:57:52 -0400

    Added Lim to the about page

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -1,10 +1,17 @@
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
 from modules.nav import SideBarLinks
+import os
 
 SideBarLinks(show_home=True)
 
 st.write("# About this App")
+
+here = os.path.dirname(__file__)
+
+st.image("assets/JoinUPresident.png", width=2000)
+
+
 
 st.markdown (
     """
