3b6d058991d27705f0237595bc6f2a4b5846852d
VOIDED <bmackmrhs@gmail.com>
2025-08-15 20:33:44 -0400

    Updated nav

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -11,7 +11,7 @@ def HomeNav():
 
 
 def AboutPageNav():
-    st.sidebar.page_link("pages/30_About.py", label="About", icon="🧠")
+    st.sidebar.page_link("pages/33_About.py", label="About", icon="🧠")
 
 
 #### ------------------------ President ------------------------
