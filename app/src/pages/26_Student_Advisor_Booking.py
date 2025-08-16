27388982dec642dae91b78053c8316acf5d7e41b
VOIDED <bmackmrhs@gmail.com>
2025-08-15 20:23:46 -0400

    Column name changes

--- app/src/pages/26_Student_Advisor_Booking.py
+++ app/src/pages/26_Student_Advisor_Booking.py
@@ -29,8 +29,14 @@ try:
             
         if data:
             df = pd.DataFrame([data])
-            st.subheader("Name and Email")
-            st.dataframe(df[['advisor_name', 'emailAddress']], use_container_width=True)
+            df.rename(columns={
+                    "advisor_name": "Advisor Name",
+                    "emailAddress": "Email Address",
+                }, inplace=True)
+            cols_to_show = [c for c in ["Advisor Name", "Email Address"] if c in df.columns]
+            if cols_to_show:
+                st.subheader("Name and Email")
+                st.dataframe(df[cols_to_show], use_container_width=True)
         else:
             st.warning("No info found - API returned empty array")
     else:
